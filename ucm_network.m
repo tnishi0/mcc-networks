9d80941ca77b232e55048325cf489da447345b59
tnishi0 <tnishi0@users.noreply.github.com>
2017-12-19 10:47:58 -0600

    Update ucm_network.m

--- ucm_network.m
+++ ucm_network.m
@@ -1,6 +1,13 @@
 function [A,n,m] = ucm_network(k,ell)
 % Constructs the UCM network for given k and ell.
 %
+% Reference:
+%
+%  T. Nishikawa, J. Sun, and A. E. Motter
+%  Sensitive Dependence of Optimal Network Dynamics on Network Structure
+%  Physical Review X 7, 041044 (2017)
+%  DOI: 10.1103/PhysRevX.7.041044
+%
 % Copyright (c) 2017 Takashi Nishikawa
 
 n = k*ell;
