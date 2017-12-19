bd446666651fd0f7776dfcd39addfffaa7279e1c
tnishi0 <tnishi0@users.noreply.github.com>
2017-12-19 10:47:14 -0600

    Update mcc_inf.m

--- mcc_inf.m
+++ mcc_inf.m
@@ -1,6 +1,13 @@
 function y = mcc_inf(phi)
 % Computes the limit of n -> infinity in Eq. (15) of the paper.
 %
+% Reference:
+%
+%  T. Nishikawa, J. Sun, and A. E. Motter
+%  Sensitive Dependence of Optimal Network Dynamics on Network Structure
+%  Physical Review X 7, 041044 (2017)
+%  DOI: 10.1103/PhysRevX.7.041044
+%
 % Copyright (c) 2017 Takashi Nishikawa
 
 ell = floor(1./(1-phi));
